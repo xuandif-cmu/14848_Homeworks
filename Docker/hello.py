5473a7a8423e37f32a3e843f6969fc3e9d8b7ff5
Xuandi FU <xuandif@andrew.cmu.edu>
2021-09-29 03:49:33 -0400

    fixed the CrashLoopBackOff error

--- Docker/hello.py
+++ Docker/hello.py
@@ -1,1 +1,15 @@
+import socket
+
 print("Hello World")
+fuxuandi = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+fuxuandi.bind(('0.0.0.0', 3000))
+fuxuandi.listen(5)
+
+
+while True:
+	try:
+		(conn, address) = fuxuandi.accept() 
+		conn.sendall("".decode("utf-8"))
+	except:
+		break
